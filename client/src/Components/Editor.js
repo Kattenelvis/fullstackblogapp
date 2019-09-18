a373b79d0d87c910248a7c250401ac8dc5a1b77f
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-18 16:40:17 +0200

    Continued work on text editor

--- client/src/Components/Editor.js
+++ client/src/Components/Editor.js
@@ -3,7 +3,19 @@ import React from "react";
 export default function Editor() {
   return (
     <div>
-      <p>Hi</p>
+      <button onClick={changeColor("blue")}>Blue</button>
+      <button onClick={changeColor("red")}>Blue</button>
+      <div
+        contentEditable
+        name="richTextField"
+        style={{
+          width: "80%",
+          height: "500px",
+          margin: "auto",
+          border: "1px solid black"
+        }}
+      ></div>
+      <button>Publish</button>
     </div>
   );
 }
