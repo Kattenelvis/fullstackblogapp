f8805f293882e2f664c9237d99390c2447ad8bf8
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-23 18:45:33 +0200

    Made images uploadable

--- client/src/Components/Editor.js
+++ client/src/Components/Editor.js
@@ -1,77 +1,94 @@
-import React from "react";
-import Axios from "axios";
+import React, { useState } from 'react'
+import Axios from 'axios'
 
-import baseURL from '../baseURL';
+import baseURL from '../baseURL'
 
 export default function Editor() {
-  const changeColor = e => {
-    document.execCommand("styleWithCSS");
-    let color = e.target.value;
-    document.execCommand("foreColor", false, color);
-    setFocus();
-  };
+	const changeColor = e => {
+		document.execCommand('styleWithCSS')
+		let color = e.target.value
+		document.execCommand('foreColor', false, color)
+		setFocus()
+	}
 
-  window.addEventListener(
-    "load",
-    () => {
-      handleButtonClicks("#bold", "Bold");
-      handleButtonClicks("#italic", "Italic");
-      handleButtonClicks("#under", "Underline");
-    },
-    false
-  );
+	window.addEventListener(
+		'load',
+		() => {
+			handleButtonClicks('#bold', 'Bold')
+			handleButtonClicks('#italic', 'Italic')
+			handleButtonClicks('#under', 'Underline')
+		},
+		false
+	)
 
-  const handleButtonClicks = (id, command) =>{
-    document.querySelector(id).addEventListener("click", () => {
-      document.execCommand(command);
-      setFocus();
-    });
-  }
+	const handleButtonClicks = (id, command) => {
+		document.querySelector(id).addEventListener('click', () => {
+			document.execCommand(command)
+			setFocus()
+		})
+	}
 
-  const publishBlog = e => {
-    e.preventDefault();
+	let currentImage = ''
+	const publishBlog = e => {
+		e.preventDefault()
+		const body = e.target.querySelector('#textField').innerHTML
+		const title = e.target[4].value
+		Axios.post(baseURL, { title, body, image: currentImage })
+	}
 
-    const body = e.target.querySelector("#textField").innerHTML;
-    const title = e.target[4].value;
-    const image = e.target[3].value;
+	const setFocus = () => {
+		document.getElementById('textField').focus()
+	}
+	function readImage(input) {
+		const reader = new FileReader()
+		reader.onload = function(e) {
+			const img = document.querySelector('#img')
+			img.setAttribute('src', e.target.result)
+			currentImage = e.target.result
+		}
+		reader.readAsDataURL(input.target.files[0])
+	}
 
-    Axios.post(baseURL, { title, body, image });
-  };
-
-  const setFocus = () => {
-    document.getElementById("textField").focus();
-  };
-
-  return (
-    <div>
-      <form onSubmit={publishBlog}>
-        <input style={{border:"none"}} type="color" onChange={changeColor} />
-        <input id="bold" type="button" value="B" className="actionButton-small" />
-        <input id="italic" type="button" value="I" className="actionButton-small" />
-        <input id="under" type="button" value="U" className="actionButton-small" />
-        <label>
-          Title:
-          <input maxLength="80" required />
-        </label>
-        <label>
-          Image Link:
-          <input required />
-        </label>
-        <div
-          id="textField"
-          contentEditable
-          name="richTextField"
-          style={{
-            width: "70%",
-            height: "500px",
-            margin: "auto",
-            border: "1px solid black",
-            float: "left"
-          }}
-        ></div>
-        <div></div>
-        <button type="submit" className="actionButton">Publish</button>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={publishBlog}>
+				<input style={{ border: 'none' }} type='color' onChange={changeColor} />
+				<input id='bold' type='button' value='B' className='actionButton-small' />
+				<input id='italic' type='button' value='I' className='actionButton-small' />
+				<input id='under' type='button' value='U' className='actionButton-small' />
+				<label>
+					Title:
+					<input maxLength='80' required />
+				</label>
+				<label>
+					Image Link:
+					<input
+						onChange={readImage}
+						accept='image/*'
+						type='file'
+						className='imageFile'
+						id='imageFile'
+					/>
+					{/* <input required /> */}
+				</label>
+				<div
+					id='textField'
+					contentEditable
+					name='richTextField'
+					style={{
+						width: '70%',
+						height: '500px',
+						margin: 'auto',
+						border: '1px solid black',
+						float: 'left'
+					}}
+				></div>
+				<div></div>
+				<button type='submit' className='actionButton'>
+					Publish
+				</button>
+			</form>
+			<img id='img' alt='' style={{ width: '200px', height: '200px' }} />
+		</div>
+	)
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,65 +1,80 @@
-const express = require("express");
-const jwt = require('jsonwebtoken');
-const router = express.Router();
-let blogPosts = require("../blogpostExamples");
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const router = express.Router()
+let blogPosts = require('../blogpostExamples')
 const BlogSchema = require('../blogSchema')
-const {verifyToken} = require('./users')
+const { verifyToken } = require('./users')
 
-router.get("/", (req, res) => {
-  BlogSchema.find().then(items => res.json(items))
-});
+const app = express()
+const fileUpload = require('express-fileupload')
+app.use(fileUpload())
 
-router.get("/:id", (req, res) => {
-  BlogSchema.findById(req.params.id).then(item => res.json(item))
-});
+router.get('/', (req, res) => {
+	BlogSchema.find().then(items => res.json(items))
+})
 
-router.post("/", verifyToken, (req, res) => {
-  let newBlog = {};
-  jwt.verify(req.token, 'secretkey', (err, data) =>{
-    if (err)  res.sendStatus(403);
-    else{
-        newBlog = new BlogSchema({
-        title:req.body.title,
-        body:req.body.body,
-        image:req.body.image,
-        likes:0
-      }) 
-      //Send to mongodb database
-      newBlog.save().then(item => res.json(item))
-    }
-  })
-});
+router.get('/:id', (req, res) => {
+	BlogSchema.findById(req.params.id).then(item => res.json(item))
+})
 
-router.patch("/:id", (req, res)  => {
-  BlogSchema.findByIdAndUpdate(req.body._id, req.body, e => {
-    res.json(e)
-  })
-});
+router.post(
+	'/',
+	/*verifyToken,*/ (req, res) => {
+		let newBlog = {}
+		//jwt.verify(req.token, "secretkey", (err, data) => {
+		//if (err) res.sendStatus(403);
+		//else {
 
-router.get("/:id/comments", (req, res) => {
-  res.json(
-    blogPosts.filter(post => post.id === parseInt(req.params.id))[0].comments
-  );
-});
+		const file = req.files
+		if (file !== undefined) {
+			file.mv(`${__dirname}/client/src/uploads/${file.name}`, err => {
+				if (err) console.error(err)
+			})
+		}
 
-router.post("/:id/comments", (req, res) => {
-  const newComment = {
-    id: randomID(),
-    name: req.body.name,
-    comment: req.body.comment,
-    date: new Date(),
-    likes:0
-  };
+		//console.log(req.body)
 
-  res.send("success");
+		newBlog = new BlogSchema({
+			title: req.body.title,
+			body: req.body.body,
+			image: req.body.image,
+			likes: 0
+		})
+		//Send to mongodb database
+		newBlog.save().then(item => res.json(item))
+		// }
+		//});
+	}
+)
 
-  blogPosts
-    .filter(post => post.id === parseInt(req.params.id))[0]
-    .comments.push(newComment);
-});
+router.patch('/:id', (req, res) => {
+	BlogSchema.findByIdAndUpdate(req.body._id, req.body, e => {
+		res.json(e)
+	})
+})
+
+router.get('/:id/comments', (req, res) => {
+	res.json(blogPosts.filter(post => post.id === parseInt(req.params.id))[0].comments)
+})
+
+router.post('/:id/comments', (req, res) => {
+	const newComment = {
+		id: randomID(),
+		name: req.body.name,
+		comment: req.body.comment,
+		date: new Date(),
+		likes: 0
+	}
+
+	res.send('success')
+
+	blogPosts
+		.filter(post => post.id === parseInt(req.params.id))[0]
+		.comments.push(newComment)
+})
 
 const randomID = () => {
-  return Math.floor(Math.random() * 500000000000000);
-};
+	return Math.floor(Math.random() * 500000000000000)
+}
 
-module.exports = router;
+module.exports = router
